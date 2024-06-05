bcd8395ebcefe2dad248db62917f118be6da90a2
gui <168765125+guisouzsa@users.noreply.github.com>
2024-06-05 10:01:13 -0300

    Update question05.py

--- question05.py
+++ question05.py
@@ -1,7 +1,7 @@
 hora = int(input(f' Que horas são?'))
 if 0 <= hora <12:
     print(f' agora é manhã!')
-if 12 <= hora <18: 
+elif 12 <= hora <18: 
     print(f' Agora é tarde!')
 if 18 <= hora <23:
     print(f' Agora é noite!')
