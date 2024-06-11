fd66245b485ae9a879cebf6212ff089ffbed0c98
guisousza <guilherme.souza103@aluno.ce.gov.br>
2024-06-10 22:15:15 -0300

    atualizado

--- question10.py
+++
@@ -1,7 +0,0 @@
-numero = int(input("Qual número deseja que apareça a tabuada?"))
-for n2 in range(1,11,1):
-    print(f'{numero} x {n2} = {numero * n2}')
-<<<<<<< HEAD
-=======
-    
->>>>>>> 1622aacde03af4f9d8e662af85eb731345f6b981
