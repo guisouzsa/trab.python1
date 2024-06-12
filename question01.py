1b8dbba82cae51847f0aeaa8dffb603df3c5c52c
guisousza <guilherme.souza103@aluno.ce.gov.br>
2024-06-11 22:55:04 -0300

    atualizado

--- question01.py
+++ question01.py
@@ -1,4 +1,4 @@
 nome = input("Qual seu primeiro nome?")
 sobrenome = input("Qual seu sobrenome?")
-nome_completo = nome + "" + sobrenome
-print(f'{nome} {sobrenome}')
+nome_completo = nome + " " + sobrenome
+print(f'{nome_completo}')
